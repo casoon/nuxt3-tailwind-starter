bd7f1cc43b70d0dfbab9644fe3ac0110db7ab8c1
casoon <joern.seidel@casoon.de>
2022-08-30 17:30:52 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-7">
+    <p class="font-medium">Nuxt 3 Starter </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useCounterStore } from '@/stores/counter'
+  const counter = useCounterStore()
+  counter.count++
+  console.log(counter.count)
+
+</script>
