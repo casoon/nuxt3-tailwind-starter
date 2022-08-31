796f74277a71083c033ed7879965ea212259eb58
casoon <joern.seidel@casoon.de>
2022-08-31 10:51:19 +0200

    directories added

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="m-7">
-    <p class="font-medium">Nuxt 3 Starter </p>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useCounterStore } from '@/stores/counter'
-  const counter = useCounterStore()
-  counter.count++
-  console.log(counter.count)
 
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/css/tailwind.css"],
-    buildModules: ['@pinia/nuxt'],
+    modules: ['@pinia/nuxt'],
     build: {
         postcss: {
             postcssOptions: {
